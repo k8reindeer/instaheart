1dc8fe8081f1f877966046ee64917cc73276f571
Kate Rudolph <k8r@mit.edu>
2013-11-05 12:00:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instaheart::Application.config.secret_token = 'f7d3d44d2e4697f77f49024e4671c321e35fb2027ad678334b7c5eb2b32ff16c01e34832e7adda21d3ed96c3dbdb86692c5b563444bfb2df395a0fc3aac656ed'
